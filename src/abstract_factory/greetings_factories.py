47fff6caf35787ff9d1517064e7d9160cf994815
Andreea Lucau <andreea.lucau@gmail.com>
2016-02-17 23:33:34 +0000

    Add another language for Abstract Factories example

--- src/abstract_factory/greetings_factories.py
+++ src/abstract_factory/greetings_factories.py
@@ -2,13 +2,14 @@
 #!/usr/bin/env python
 # pylint: disable=too-few-public-methods
 
+import abc
 import argparse
 
 
 class AbstractGreetingsFactory(object):
     """Abstract factory for greetings.
 
-    It implements two methods: say_hello and say_goodbye, that return Hello and
+    It implements two methods: get_hello and get_goodbye, that return Hello and
     Goodbye objects.
     """
 
@@ -17,34 +18,32 @@ class AbstractGreetingsFactory(object):
 
         The classes that inherit from it have to implement get.
         """
-        def get(self):
-            """Return the hello message.
+        __metaclass__ = abc.ABCMeta
 
-            Raises:
-                NotImplementedError
-            """
-            raise NotImplementedError()
+        @abc.abstractmethod
+        def get(self):
+            """Return the hello message."""
+            pass
 
     class Goodbye(object):
         """Abstract class for saying goodbye.
 
         The classes that inherit from it have to implement get.
         """
-        def get(self):
-            """Return the goodbye message.
+        __metaclass__ = abc.ABCMeta
 
-            Raises:
-                NotImplementedError
-            """
-            raise NotImplementedError()
+        @abc.abstractmethod
+        def get(self):
+            """Return the goodbye message."""
+            pass
 
     @classmethod
-    def say_hello(cls):
+    def get_hello(cls):
         """Create a Hello class."""
         return cls.Hello()
 
     @classmethod
-    def say_goodbye(cls):
+    def get_goodbye(cls):
         """Create a Goodbye class."""
         return cls.Goodbye()
 
@@ -85,14 +84,35 @@ class RomanianGreetingsFactory(AbstractGreetingsFactory):
             return "Papapapapa"
 
 
+class FrenchGreetingsFactory(AbstractGreetingsFactory):
+    """French Greetings Factory."""
+
+    class Hello(AbstractGreetingsFactory.Hello):
+        """Say Hello in French."""
+
+        def get(self):
+            """Return the hello message string."""
+            return "Bonjour"
+
+    class Goodbye(AbstractGreetingsFactory.Goodbye):
+        """Say Goodbye in French."""
+
+        def get(self):
+            """Return the goodbye message string."""
+            return "Au revoir"
+
+
 def say_hello_and_goodbye(language):
     """Print language-specifc hello and goodbye messages."""
     if language == "en":
-        hello = EnglishGreetingsFactory.say_hello()
-        goodbye = EnglishGreetingsFactory.say_goodbye()
+        hello = EnglishGreetingsFactory.get_hello()
+        goodbye = EnglishGreetingsFactory.get_goodbye()
     elif language == "ro":
-        hello = RomanianGreetingsFactory.say_hello()
-        goodbye = RomanianGreetingsFactory.say_goodbye()
+        hello = RomanianGreetingsFactory.get_hello()
+        goodbye = RomanianGreetingsFactory.get_goodbye()
+    elif language == "fr":
+        hello = FrenchGreetingsFactory.get_hello()
+        goodbye = FrenchGreetingsFactory.get_goodbye()
 
     print hello.get()
     print goodbye.get()
@@ -104,7 +124,8 @@ def parse_args():
     The only accepted parameter is "language".
     """
     parser = argparse.ArgumentParser()
-    parser.add_argument("language", metavar="language", choices=["en", "ro"])
+    parser.add_argument("language", metavar="language",
+                        choices=["en", "ro", "fr"])
     return parser.parse_args()
 
 
